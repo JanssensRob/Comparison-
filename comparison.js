1b27e55493e456882f70ac6739b549167e5aab85
Rob <robjanssens93@hotmail.com>
2021-03-02 11:01:31 +0100

    Add files via upload

---
+++ comparison.js
@@ -0,0 +1,29 @@
+let b = 17;
+
+function between(b, min, max) {
+    return b >= min && x <= max;
+}
+
+if (between(b, 18, 25)) { console.log("Je krijgt 50% korting!") }
+else { console.log("Je krijgt lekker helemaal niks noppes") };
+
+const naam = "Bram";
+
+if (naam === "Sarah" || naam === "Bram") { console.log("Je krijgt een gratis biertje") }
+else { console.log("Helaas pindakaas geen biertje") };
+
+
+
+const totalAmount = 30;
+
+function above(totalAmount, min, totalAmount, max) {
+    return totalAmount >= min && totalAmount <= max;
+}
+
+if (above(totalAmount, 25, totalAmount, 49)) { console.log("Yum, je krijgt gratis vega-bitterballs") }
+else { console.log("Helaas, je hebt geen gratis vegaballs"); }
+if (above(totalAmount, 50, totalAmount, 99)) { console.log("YUM, je krijgt portie nachos erbij") }
+else { console.log("Nope, only vegaballs for you") };
+if (above(totalAmount, 100, totalAmount, 10000000)) { console.log("OMG GRAB THE CHAMPAGNE") }
+else { console.log("No champagne for you boy") };
+
